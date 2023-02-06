0c3185750a799cbce052303a2d92f3a4b0faba0e
EC2 Default User <ec2-user@ip-172-31-10-136.ap-northeast-1.compute.internal>
2023-02-06 16:40:56 +0000

    ログイン機能、ヘッダー・フッター実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,27 @@
 Rails.application.routes.draw do
+  # 管理者用
+  # URL /admin/sign_in ...
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
+
+  # 顧客用
+  # URL /customers/sign_in ...
+  devise_for :customers,skip: [:registrations, :passwords], controllers: {
+    sessions: 'public/sessions'
+  }
+
+  #管理者側ルーティング設定
+  namespace :admin do
+    get "/" => "homes#top"
+  end
+
+  #会員側ルーティング設定
+  scope module: :public do
+    root "homes#top"
+    get 'homes/about' => "homes#about", as: "about"
+  end
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,40 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#管理者ログイン用
+#Admin内を探す：既にsample-email@adminが作られていないか
+admin = Admin.find_by(email: 'sample-email@admin')
+#もし作られていなかったら、新しく作成
+Admin.create!(email: 'sample-email@admin',password: 'admin1') if admin.blank?
+
+#会員ログイン用
+customer = Customer.find_by(email: 'sample@one')
+if customer.blank?
+   Customer.create!(
+      first_name: '一',
+      last_name: '一郎',
+      first_name_kana: 'イチ',
+      last_name_kana: 'イチロウ',
+      address: '一県',
+      phone_number: "11111111111",
+      email: 'sample@one',
+      password: 'customer1',
+      is_active: true
+   )
+end
+
+customer = Customer.find_by(email: 'sample@tow')
+if customer.blank?
+   Customer.create!(
+      first_name: '二',
+      last_name: '二郎',
+      first_name_kana: 'ニ',
+      last_name_kana: 'ジロウ',
+      address: '二県',
+      phone_number: "22222222222",
+      email: 'sample@tow',
+      password: 'customer2',
+      is_active: true
+   )
+end
