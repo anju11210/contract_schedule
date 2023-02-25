c8f7d2a1b7b6092dce7a7071542add26b67eb282
EC2 Default User <ec2-user@ip-172-31-10-136.ap-northeast-1.compute.internal>
2023-02-25 08:46:51 +0000

    timeセレクトボックス編集

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,5 +1,6 @@
 class Appointment < ApplicationRecord
   belongs_to :customer
+  before_save :set_the_day_implement
 
   with_options presence: true do
     validates :name
@@ -17,5 +18,16 @@ class Appointment < ApplicationRecord
     validates :other_expense_3
     validates :other_expense_4
     validates :other_expense_5
+  end
+
+  private
+
+  def set_the_day_implement
+    #dateのフォームから日付を取得
+    year = self.date.year
+    month = self.date.month
+    day = self.date.day
+    #フォームから取得した値をtimeに挿入
+    self.time = self.time.change(year: year, month: month, day: day)
   end
 end
